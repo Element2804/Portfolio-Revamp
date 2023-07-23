2959c66c27154bab8784e845a3699c9cf62fb235
Element2804 <103654389+Element2804@users.noreply.github.com>
2023-07-23 15:01:47 -0400

    added some pages

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,11 @@
 import React from "react";
-// import profileImage from "../../assets/large/Profile/Profile_Pic.png";
+
 
 function About() {
   return (
     <section class="about" id="about_link">
-      <div class="flex-row">
-        <h1 id="about" className="section-title primary-border">
-          About Me
-        </h1>
-      </div>
-      {/* <div className="flex-row">
-        <div class="intro-info">
-          <div class="intro-img">
-            <img src={profileImage} style={{ width: "100%" }} alt="profile" />
-          </div> */}
-         
+      
+               
         <h2>About me</h2>
 
         <p>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import NavTabs from './components/NavTabs';
+import About from './components/About';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <NavTabs /> <About /> <App />
   </React.StrictMode>
 );
 
