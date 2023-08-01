d58337c6a32d14f504f5174c6fc799d85a82651f
Element2804 <103654389+Element2804@users.noreply.github.com>
2023-08-01 14:04:55 -0400

    added resume download

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,23 @@
+
+
+
 const Resume = () => {
     return ( 
         <section>
             <h2>Resume</h2>
-        
-
-
+        <h3>Development Skills</h3>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+            <li>Mongo Database</li>
+            <li>SQL</li>
+            <li>ExpressJS</li>
+            <li>React</li>
+        </ul>
+        <a href={require("../../components/file/RESUME.pdf")} download>
+        <button>Download Resume</button>
+        </a>
             </section>
         
 
