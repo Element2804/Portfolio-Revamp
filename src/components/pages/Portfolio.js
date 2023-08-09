b0e57b413de61ecf798efd03d4cdeaccc4d16b73
Element2804 <103654389+Element2804@users.noreply.github.com>
2023-08-08 21:58:35 -0400

    added group project 3

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,6 +1,7 @@
 import React from "react";
 import personnel from "../../images/Personnel.png"
 import pwGen from "../../images/PWgen.png"
+import gulp from "../../images/Gulpco.png"
 
 const Portfolio = () => {
     return (
@@ -11,7 +12,10 @@ const Portfolio = () => {
                 <img src ={personnel} className="person" alt="personnel tracker"/>
                 </a>
                <a href="https://github.com/Element2804/pink-kittens">
-                <img src ={pwGen} className="person" alt="personnel tracker"/>
+                <img src ={pwGen} className="person" alt="password generator"/>
+                </a>
+               <a href="https://gulpco-9498db2ffc8b.herokuapp.com/">
+                <img src ={gulp} className="person" alt="drink ordering website"/>
                 </a>
 
             </div>
